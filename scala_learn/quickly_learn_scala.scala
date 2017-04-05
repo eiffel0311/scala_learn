eabc90e56c5397e82dcebe3bdf9c25a39a4e1bd6
艾飞 <aif@yiche.com>
2017-04-05 19:10:24 +0800

    .

--- scala_learn/quickly_learn_scala.scala
+++ scala_learn/quickly_learn_scala.scala
@@ -1,14 +1,24 @@
 /*
 第一章 基础
 */
-// 1.1 变量, 常量
+// 1 变量, 常量
 var a = 1
-println(a)
 val b = 1
-println(b)
+
+// 2 常用数据类型
+var byte: Byte = 1
+var char: Char = 'c'
+var short: Short = 1
+var int: Int = 1
+var long: Long = 1
+var float: Float = 1
+var double: Double = 1
 
 
+/*
+第二章 控制结构和函数
+*/
+// if else
 
-//第二章 控制结构和函数
 //
 //第三章 数组和相关操作
