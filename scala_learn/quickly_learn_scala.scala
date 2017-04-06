38a31c1fd1263910f46d98b0e13b41e7e93525a3
艾飞 <aif@yiche.com>
2017-04-06 09:42:36 +0800

    .

--- scala_learn/quickly_learn_scala.scala
+++ scala_learn/quickly_learn_scala.scala
@@ -14,11 +14,66 @@ var long: Long = 1
 var float: Float = 1
 var double: Double = 1
 
+val boolean: Boolean = false
+
 
 /*
 第二章 控制结构和函数
 */
-// if else
+// 1 if else
+var tag = 1
+if (tag == 1)
+  println("if")
+else if (tag == 2)
+  println("else if")
+else
+  println("else")
+
+// 2 代码块, 代码块返回值
+var block = {if (1==1) 1 else 2}
+println(block)
+
+// 3 while
+var while_index = 1
+while (while_index < 3){
+  println("i am while_index: " + while_index)
+  while_index = while_index + 1
+}
+
+// 4 for
+// --------------基本for循环
+for(i <- 1 to 3){
+  println("i am for index: " + i)
+}
+
+for(i <- 0 until 3){
+  println("i am for index2: " + i)
+}
+
+for(i <- Range(0, 3)){
+  println("i am for index3: " + i)
+}
+
+for(i <- "abcd"){
+  println("i am for index4: " + i)
+}
+// --------------基本for循环
+
+// --------------高级for循环， 推导式
+// 循环嵌套
+for(i <- 1 to 2; j <- 1 to 3){printf("%d + %d = %d\n", i, j, i + j)}
+// 循环守卫
+for(i <- 1 to 2; j <- 1 to 3 if i == j){printf("%d + %d = %d\n", i, j, i + j)}
+// 循环中随意定义
+for(i <- 1 to 2; from = i; j <- from to 3){printf("%d + %d = %d\n", i, j, i + j)}
+// 循环构造集合
+var for_vector = for(i <- 1 to 2; j <- 1 to 3) yield i + j
+println(for_vector)
+// --------------高级for循环， 推导式
+
+// 5 函数
+
+
 
 //
 //第三章 数组和相关操作
